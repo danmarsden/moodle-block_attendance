287c606b41d984b9dd90c14d286ee4186ea168d1
Dan Marsden <dan@danmarsden.com>
2013-07-03 20:42:19 +1200

    remove old lang in preparation for inclusion in AMOS - tidy up readme/text

--- lang/sv/block_attendance.php
+++
@@ -1,7 +0,0 @@
-<?PHP // $Id: block_attendance.php,v 1.1.10.1 2009/02/23 19:18:57 dlnsk Exp $ 
-      // block_attendance.php - created with Moodle 1.6 development (2005101200)
-
-
-$string['blockname'] = 'Närvaro';
-
-?>
