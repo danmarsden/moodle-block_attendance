379d4dc932a8757b1d03007a9a80dc5cb9a60261
Dan Marsden <dan@danmarsden.com>
2013-07-04 14:16:44 +1200

    set correct version dependency on attendance mod

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 $plugin->version = 2013070301;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_attendance';
-$plugin->dependencies = array('mod_attendance' => 2012120700);
+$plugin->dependencies = array('mod_attendance' => 2013070400);
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '2.4';
