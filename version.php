a411381bccb7cf115725fcfdc47f548a56c16722
Dan Marsden <dan@danmarsden.com>
2014-12-05 09:19:32 +1300

    bump version for string update

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013070302;
+$plugin->version = 2013070303;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_attendance';
 $plugin->dependencies = array('mod_attendance' => 2013082904);
