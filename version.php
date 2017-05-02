29104fed69aa416f4b99d8698bdf6354d0de7ed8
Dan Marsden <dan@danmarsden.com>
2017-05-02 12:50:39 +1200

    fix travis, add some checks.

--- version.php
+++ version.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $plugin->version = 2017020700;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_attendance';
